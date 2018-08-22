1855af64a170e8ac6f8f550270a6a5a97bb4d768
Manuel <mlm09kdev@gmail.com>
2018-08-21 21:37:19 -0400

    Playing with animation and progress bars on Kotlin

--- app/src/main/java/com/mlm09kdev/kotlinpracticeapp/MainActivity.kt
+++ app/src/main/java/com/mlm09kdev/kotlinpracticeapp/MainActivity.kt
@@ -3,7 +3,7 @@ package com.mlm09kdev.kotlinpracticeapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
+import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -11,15 +11,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button1.setOnClickListener{
-            change(imageView)
+
+        val initialTextViewTranslationY = progressTextView.translationY
+
+        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                progressTextView.text = progress.toString()
+
+                val translationDistance = (initialTextViewTranslationY - progress * resources.getDimension(R.dimen.text_anim_step) * -1)
+
+                progressTextView.animate().translationX(translationDistance)
+                if(!fromUser)
+                    progressTextView.animate().setDuration(500).rotationBy(360f).translationY(initialTextViewTranslationY)
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+
+        })
+
+        resetButton.setOnClickListener{
+            seekBar.progress = 0
+
         }
 
     }
 
-   private fun change( view: View){
-       imageView.setImageResource(R.drawable.ic_launcher_foreground)
-    }
+
 }
 
 
