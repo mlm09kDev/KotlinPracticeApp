717f7076a1c7d876fefdd6bcc166c05054c8f22a
Manuel <mlm09kdev@gmail.com>
2018-08-22 21:53:54 -0400

    working a timer practice app.

--- app/src/main/java/com/mlm09kdev/kotlinpracticeapp/MainActivity.kt
+++ app/src/main/java/com/mlm09kdev/kotlinpracticeapp/MainActivity.kt
@@ -2,45 +2,107 @@ package com.mlm09kdev.kotlinpracticeapp
 
 
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.SeekBar
+import android.widget.TimePicker
+import com.mlm09kdev.kotlinpracticeapp.utils.PreferenceUtils
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    enum class  TimerState{
+        Stopped, Paused, Running
+    }
+
+    private lateinit var  timer: CountDownTimer
+    private var timerLengthSeconds = 0L
+    private var timerState: TimerState = TimerState.Stopped
+
+    private var secondsRemaining = 0L
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.setIcon(R.drawable.ic_timer)
+        supportActionBar?.title = "     Timer"
 
-        val initialTextViewTranslationY = progressTextView.translationY
+        fab_start_button.setOnClickListener { v ->
+            startTimer()
+            timerState = TimerState.Running
+            updateButtons()
 
-        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                progressTextView.text = progress.toString()
+        }
+        fab_pause_button.setOnClickListener { v ->
+            timer.cancel()
+            timerState = TimerState.Paused
+            updateButtons()
+        }
 
-                val translationDistance = (initialTextViewTranslationY - progress * resources.getDimension(R.dimen.text_anim_step) * -1)
-
-                progressTextView.animate().translationX(translationDistance)
-                if(!fromUser)
-                    progressTextView.animate().setDuration(500).rotationBy(360f).translationY(initialTextViewTranslationY)
-            }
-
-            override fun onStartTrackingTouch(p0: SeekBar?) {
-            }
-
-            override fun onStopTrackingTouch(p0: SeekBar?) {
-            }
-
-        })
-
-        resetButton.setOnClickListener{
-            seekBar.progress = 0
-
+        fab_stop_button.setOnClickListener { v ->
+            timer.cancel()
+            onTimerFinished()
         }
 
     }
 
+    override fun onResume() {
+        super.onResume()
+    initTimer()
+    }
 
+    override fun onPause() {
+        super.onPause()
+
+        if(timerState == TimerState.Running){
+            timer.cancel()
+
+        }
+        else if (timerState == TimerState.Paused)
+        {
+
+        }
+        PreferenceUtils.setPreviousTimerLengthSeconds(timerLengthSeconds, this)
+        PreferenceUtils.setSecondsRemaining(secondsRemaining, this)
+        PreferenceUtils.setTimerState(timerState, this)
+    }
+
+    fun initTimer(){
+        timerState = PreferenceUtils.getTimerState(this)
+        if(timerState == TimerState.Stopped){
+            setNewTimerLength()
+        }
+        else{
+            setPreviousTimerLength()
+        }
+
+        secondsRemaining = if(timerState == TimerState.Running || timerState == TimerState.Paused)
+            PreferenceUtils.getSecondsRemaining(this)
+        else
+            timerLengthSeconds
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_layout, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
 
 
