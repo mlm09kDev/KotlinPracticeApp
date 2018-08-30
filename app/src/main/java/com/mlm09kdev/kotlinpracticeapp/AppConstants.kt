0863ae3c0a1b2496d5e3bb7f25738d076e872fd0
Manuel <mlm09kdev@gmail.com>
2018-08-29 21:37:57 -0400

    added the first steps to working notifications

---
+++ app/src/main/java/com/mlm09kdev/kotlinpracticeapp/AppConstants.kt
@@ -0,0 +1,14 @@
+package com.mlm09kdev.kotlinpracticeapp
+
+/**
+ * Created by Manuel Montes de Oca on 8/29/2018.
+ */
+
+class AppConstants {
+    companion object {
+        const val ACTION_STOP = "stop"
+        const val ACTION_PAUSE = "pause"
+        const val ACTION_RESUME = "resume"
+        const val ACTION_START = "start"
+    }
+}
